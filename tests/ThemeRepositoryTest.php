7ef54f52356ef1cd5232c652660c25767a0bafba
fridev <magrigsdesign@gmail.com>
2025-01-17 00:04:16 +0100

    successed tests for creating entity theme and adding data in database

---
+++ tests/ThemeRepositoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Entity\Theme;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ThemeRepositoryTest extends KernelTestCase
+{
+    public function testAddThemeToDatabase():void 
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $entityManager = $container->get('doctrine.orm.entity_manager');
+
+        $theme = new Theme();
+        $theme->setParentId(20);
+
+        $entityManager->persist($theme);
+        $entityManager->flush();
+
+        $themeRepository = $entityManager->getRepository(Theme::class);
+        $savedTheme = $themeRepository->find($theme->getId());
+
+        $this->assertNotNull($savedTheme);
+        $this->assertSame(20, $savedTheme->getParentId());
+    }
+}
