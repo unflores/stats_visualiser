07787e5ecdda6182fbb07f9c5526320d2ec53edb
fridev <magrigsdesign@gmail.com>
2025-02-07 12:55:31 +0100

    test ThemeRepositoryTest

--- tests/ThemeRepositoryTest.php
+++ tests/ThemeRepositoryTest.php
@@ -7,25 +7,48 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ThemeRepositoryTest extends KernelTestCase
 {
-    public function testAddThemeToDatabase(): void
+
+    public function testAddParentTheme(): void
     {
-        self::bootKernel();
-        $container = static::getContainer();
+       self::bootKernel();
+       $container = static::getContainer();
 
         $entityManager = $container->get('doctrine.orm.entity_manager');
-
         $theme = new Theme();
-        $theme->setParentId(20);
-
+        $theme->setCode('environnement');
+        $theme->setIsSection(true);
+        $theme->setParentId(null);
+        $theme->setExternalId('2980');
         $entityManager->persist($theme);
         $entityManager->flush();
-
+        
         $themeRepository = $entityManager->getRepository(Theme::class);
         $savedTheme = $themeRepository->find($theme->getId());
 
         $this->assertNotNull($savedTheme);
-        $this->assertSame(20, $savedTheme->getParentId());
+        $this->assertSame('environnement', $savedTheme->getCode());
+
     }
 
-    
+    public function testAddChildTheme():void
+    {
+        self::bootKernel();
+       $container = static::getContainer();
+
+        $entityManager = $container->get('doctrine.orm.entity_manager');
+        $theme = new Theme();
+        $theme->setCode('Emissions GES');
+        $theme->setIsSection(true);
+        $theme->setParentId(1);
+        $theme->setExternalId('2981');
+        $entityManager->persist($theme);
+        $entityManager->flush();
+        
+        $themeRepository = $entityManager->getRepository(Theme::class);
+        $savedTheme = $themeRepository->find($theme->getId());
+
+        $this->assertNotNull($savedTheme);
+        $this->assertSame(1, $savedTheme->getParentId());
+        $this->assertSame("Emissions GES", $savedTheme->getCode());
+    }
 }
