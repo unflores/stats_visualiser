9a9a96441a2e9dc92206748fd18ab4eedc4d4f7d
magrigsdev <magrigsdesign@gmail.com>
2025-03-04 01:25:24 +0100

    The migration Version20250116221307 has been successfully added.

--- migrations/Version20250221095203.php
+++ migrations/Version20250221095203.php
@@ -14,20 +14,28 @@ final class Version20250221095203 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add columns and indexes into theme table';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE theme (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, parent_id INTEGER DEFAULT NULL, code VARCHAR(255) NOT NULL, is_section BOOLEAN NOT NULL, external_id VARCHAR(255) NOT NULL)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_9775E70877153098 ON theme (code)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_9775E7089F75D7B0 ON theme (external_id)');
+        
+        $this->addSql('ALTER TABLE theme ADD COLUMN is_section BOOLEAN NOT NULL');
+        $this->addSql('ALTER TABLE theme ADD COLUMN code VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE theme ADD COLUMN external_id VARCHAR(255) NOT NULL');
+        
+        $this->addSql('CREATE UNIQUE INDEX IF NOT EXISTS UNIQ_9775E70877153098 ON theme (code)');
+        $this->addSql('CREATE UNIQUE INDEX IF NOT EXISTS UNIQ_9775E7089F75D7B0 ON theme (external_id)');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE theme');
+        $this->addSql('ALTER TABLE theme DROP COLUMN parent_id');
+        $this->addSql('ALTER TABLE theme DROP COLUMN is_section');
+        $this->addSql('ALTER TABLE theme DROP COLUMN code');
+        
+        $this->addSql('DROP INDEX IF EXISTS UNIQ_9775E70877153098');
+        $this->addSql('DROP INDEX IF EXISTS UNIQ_9775E7089F75D7B0');
     }
 }
+
